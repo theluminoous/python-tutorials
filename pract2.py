f45d0cc888a8c276248b9ebf4291e1a3779e789c
AzharSaleem <azharsaleem267@gmail.com>
2023-09-08 00:04:04 +0500

    done

--- pract2.py
+++ pract2.py
@@ -1,10 +1,29 @@
-z = int(input("Enter the value of Table :"))
-for x in range(1,15):
-    if(x==10):
-      print("Out of loop")
-      continue
-    print(z," X" , x,"=", x * z)
+from math import pi
+# z = int(input("Enter the value of Table :"))
+# for x in range(1,15):
+#     if(x==10):
+#       print("Out of loop")
+#       continue
+#     print(z," X" , x,"=", x * z)
+
+name = "Azhar"
+job= 0
+if name == "Azhar" or job == 0:
+  print ("Corret")
+else:
+  print("Wrong")
 
 
+# print("Fint the area of circle")
+# r = float(input("Enter the Radius :"))
+# print("Area of circle is :", pi*r**2)
+
+# fname = input("First Name :")
+# Lname = input("Last Name :")
+# print( "Hello " + fname +" "+ Lname)
+
+SplitW = input("Input the values :")
+list = SplitW.split(",")
+print("List" , list)
 
 
